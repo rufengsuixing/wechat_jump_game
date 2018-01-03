bd000951cee880b5563236880ab3a0d51682aad0
rufengsuixing <1985151814@qq.com>
2018-01-03 20:47:50 +0800

    Add files via upload

--- wechat_jump_auto.py
+++ wechat_jump_auto.py
@@ -36,17 +36,21 @@ def get_magicnumber():
     pull_screenshot()
     im = Image.open('./autojump.png')
     # 获取棋子和 board 的位置
-    xcha1 = find_piece_and_board(im)
+    x1,y1 = find_piece_and_board(im,True)
+    ts = int(time.time())
+    debug.backup_screenshot(ts)
     set_button_position(im)
     jump(0) #100ms
     time.sleep(1)
     pull_screenshot()
     im = Image.open('./autojump.png')
     # 获取棋子和 board 的位置
-    xcha2 = find_piece_and_board(im)
+    x2,y2 = find_piece_and_board(im,True)
+    ts = int(time.time())
+    debug.save_debug_screenshot(ts, im, x1, y1, x2,y2)
     global press_coefficient
-    press_coefficient=100/(xcha1-xcha2) #100ms 的距离
-    
+    press_coefficient=150/abs(x2-x1) #100ms 的距离
+    print('number',press_coefficient)
 
 def pull_screenshot():
     '''
@@ -86,7 +90,7 @@ def jump(distance):
     跳跃一定的距离
     '''
     press_time = distance * press_coefficient
-    press_time = max(press_time, 100)   # 设置 100ms 是最小的按压时间
+    press_time = max(press_time, 150)   # 设置 100ms 是最小的按压时间
     press_time = int(press_time)
     cmd = 'adb shell input swipe {x1} {y1} {x2} {y2} {duration}'.format(
         x1=swipe_x1,
@@ -104,8 +108,16 @@ def pipei(l1,l2,cha):
         if abs(l1[a]-l2[a])>cha:
             return False
     return True
-            
-def find_piece_and_board(im):
+def pipei2(l1,l2,can=0.01):
+    ji=l2[0]/l1[0]
+    for a in range(1,3):
+        if (1+can)*ji>l2[a]/l1[a]>(1-can)*ji:
+            continue
+        break
+    else:
+        return True
+    return False            
+def find_piece_and_board(im,r=False):
     '''
     寻找关键坐标
     '''
@@ -115,7 +127,6 @@ def find_piece_and_board(im):
     piece_y = 0
     board_x = 0
     board_y = 0
-    scan_x_border = int(w / 8)  # 扫描棋子时的左右边界
     scan_start_y = int(h/3)  # 扫描的起始 y 坐标
     scan_stop_y = int(h*2/3)  # 扫描的终止 y 坐标
     scan_start_x = int(w/9)  # 扫描的起始 x 坐标
@@ -124,38 +135,128 @@ def find_piece_and_board(im):
     #get 图片中一个紫色坐标
     for x in range(scan_stop_x,scan_start_x,-50):
         for y in range(scan_stop_y,scan_start_y,-100):
-            if pipei([57,50,89],im_pixel[x,y],19):
+            if pipei([57,50,89],im_pixel[x,y],15):
                 break
         else:
             continue
         break
     #print('qizi',x,y)
     #get 左边缘
+    yuan=im_pixel[x,y]
     for x1 in range(x,scan_start_x,-1):
-        if not pipei([57,50,89],im_pixel[x1,y],19):
-                for yp in range(-10,15,1):
-                    if pipei([57,50,89],im_pixel[x1,y+yp],19):
-                        y=y+yp
-                        break
-                else:
+        if not pipei(yuan,im_pixel[x1,y],15):
+            for yp in range(-10,15,1):
+                if pipei(yuan,im_pixel[x1,y+yp],15):
+                    y=y+yp
                     break
+            else:
+                break
+        yuan=im_pixel[x1,y]
     x=x1+1
+    
+    yuan=im_pixel[x,y]
     for y1 in range(y,y+10,1):
-        if not pipei([57,50,89],im_pixel[x,y1],19):
+        if not pipei(yuan,im_pixel[x,y1],15):
             break
+        yuan=im_pixel[x,y1]
     y=y1-1
     #get 右边缘
+    yuan=im_pixel[x,y]
     for x1 in range(x,scan_stop_x,1):
-        if not pipei([57,50,89],im_pixel[x1,y],19):
+        if not pipei(yuan,im_pixel[x1,y],15):
             break
+        yuan=im_pixel[x1,y]
     x1-=1
     #左边缘x,y 右边缘 x1,y
+    zx=x
+    yx=x1
     piece_w=x1-x
-    piece_x= int((x+x1)/2)
+    piece_x= (x+x1)/2
     piece_y = y
     #棋子ok
     print('qizi',piece_x,piece_y)
+    if r==True:
+        return piece_x,piece_y
     
+    #贴边法 140，70 取70，35
+    for y in range(scan_start_y,scan_stop_y,35):
+        bj_ys=im_pixel[10,y]
+        for x in range(scan_stop_x,scan_start_x,-70):
+            #print(x,y)
+            if bj_ys!=im_pixel[x,y] and not zx<x<yx:
+                break #找到方块上点 x,y
+        else:
+            continue
+        break
+    print('suid',x,y,bj_ys,im_pixel[x,y])
+    
+    #上点(最后一个，同背景色)
+    for y1 in range(y,y-35,-1):
+        if im_pixel[x,y1]==im_pixel[10,y1]:
+            break
+        
+    print('sd',x,y1)
+    for y in range(y1,y1-150,-1):
+        for xp in range(0,10):
+            #print(x,xp,y)
+            if im_pixel[x+xp,y]!=im_pixel[10,y]:
+                x+=xp
+                break
+            if im_pixel[x-xp,y]!=im_pixel[10,y]:
+                x-=xp
+                break
+        else:
+            break
+                       
+    #x,y 最高的(beijingse)
+    y+=1
+    zuo=0
+    you=0
+    for xp in range(0,15):
+        if im_pixel[x+xp,y]!=im_pixel[10,y]:
+            you=xp
+            
+        if im_pixel[x-xp,y]!=im_pixel[10,y]:
+            zuo=xp
+    dx=(zuo+you)/2+x
+    
+    #dx,y 顶点
+    syb=y
+    print("顶点",dx,y)
+    for xp in range(0,340):
+        #int((300-xp)/269*156)
+        for yp in range(0,10):
+            #print([dx-xp,y+yp])
+            if im_pixel[dx-xp,y+yp]!=im_pixel[10,y+yp]:
+                #print("bu")
+                if pipei2(im_pixel[10,y+yp],im_pixel[dx-xp,y+yp]):
+                    
+                    continue
+                y+=yp-1
+                break
+        else:
+            break
+    
+    # x-xp,y+yp 左点
+    zx=dx-xp
+    zy=y
+    print("zuo点",zx,zy)
+    sy=syb
+    for xp in range(0,340):
+        for yp in range(0,5):
+            if im_pixel[dx+xp,sy+yp]!=im_pixel[10,sy+yp]:
+                sy+=yp-1
+                break
+        else:
+            break
+    yx=dx+xp
+    yy=sy
+    #右点
+    print("you点",yx,yy)
+    ts = int(time.time())
+    debug.save_debug_screenshot(ts, im, piece_x, piece_y, int((zx+yx)/2), int((zy+yy)/2))
+    return abs((zx+yx)/2-piece_x)
+    '''
     #269.156
     if piece_x>w/2: #棋子右半边
         xs=10
@@ -174,6 +275,15 @@ def find_piece_and_board(im):
             break 
     print(x,y,x_m)
     #get x,y
+    
+    
+    
+    
+    
+    
+    
+    
+    
     bj_ys=list(im_pixel[10,y])
     wuxiao=x+274*x_m
     for x1 in range(x+274*x_m,x,-x_m):
@@ -189,7 +299,8 @@ def find_piece_and_board(im):
     print(x1,y)
     x=(x+x1)/2
     xcha=abs(x-piece_x)
-    return xcha
+    '''
+    
 
 
 def check_screenshot():
@@ -261,7 +372,7 @@ def main():
                 time.sleep(1)
             print('\n继续')
             i, next_rest, next_rest_time = 0, random.randrange(30, 100), random.randrange(10, 60)
-        time.sleep(random.uniform(0.9, 1.2))   # 为了保证截图的时候应落稳了，多延迟一会儿，随机值防 ban
+        time.sleep(random.uniform(1, 1.3))   # 为了保证截图的时候应落稳了，多延迟一会儿，随机值防 ban
 
 
 if __name__ == '__main__':
